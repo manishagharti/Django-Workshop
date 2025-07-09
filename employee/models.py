6522a53aa270bb3d00b7ffc852ee4e1218e9ebc4
Mani_Sha <manishagg111111@gmail.com>
2025-07-09 16:31:18 +0545

    employee

---
+++ employee/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+class Employee(models.Model):
+    first_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=12, blank=True, null=True)
+    image=models.ImageField(upload_to='images')
+
+    
+    
